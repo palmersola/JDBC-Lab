01a68570eea6061106d2d20768d90d59f06d6510
palmersola <43118254+palmersola@users.noreply.github.com>
2023-06-21 14:20:48 -0500

    Update DatabaseConnector.java

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -6,7 +6,7 @@ public class DatabaseConnector {
         try {
             String url = "jdbc:mysql://localhost:3306/Bookstore";
             String username = "root";
-            String password = "Royals&19";
+            String password = "Palmer98";
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Successfully connected to the database!");
         } catch (SQLException e) {
